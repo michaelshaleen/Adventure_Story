f98b22f533dfebf408f93197c628e6a6b3ee8065
michaelshaleen <shaleenmichael@yahoo.com>
2022-01-10 13:47:30 -0600

    prints not working in code flow

--- cya.py
+++ cya.py
@@ -1,31 +1,69 @@
 import random
 name = input("Greetings! What's your name? ")
-swim_options = ["you forgot you can't swim", "you remember your swimming lessons from your youth and\
-  you power across the river easily!", "you swim to the other side of the river and see the feet of a tribal\
-    warrior", "you dive into the water, and while swimming you become covered in huge black leeches!\
-      as you leave the water you are dizzy and fall down from blood loss"]
+swim_options = ["you forgot you can't swim", 
+"you remember your swimming lessons from your youth and\
+  you power across the river easily!", 
+  "you swim to the other side of the river and see the feet of a tribal warrior.",
+    "you dive into the water, and while swimming you become covered in huge black leeches!\
+      as you leave the water you are dizzy and fall down from blood loss", "while swimming you cramp\
+        but remember you're not a quitter so you power through perfectly fine.",\
+          "Swimming sure is fun, you arrive on shore shortly."]
+
+walk_options = ["while walking you end up feeling ill, and actually pass out from heat exhaustion."\
+  , "during a lovely walk through an enchanted forest you are greeted by an elf!", "Walking around a\
+    river takes a long time.",]
+
+print(len(swim_options), "swim options")
+print(swim_options[0])
+print(len(walk_options), "walk options")
 
 random_swim = random.randint(0, 3)
-print("Welcome" , name , "this is sure to be a wild ride!")
+print("Welcome," , name , ",this is sure to be a wild ride!")
 # commas add a space automatically
-
-answer = input("You are on a dirt road, your only options are left or right \
+answer = input("You are on a dirt road, your only options are left or right\
   which way would you like to go? ").lower()
 
+
+# user decides to go left from dirt road
 if answer == "left":
   answer = input("You arrive at a river. You can walk around it or swim through it.\
     Type walk or swim: ").lower()
   if answer == "swim":
-    print(swim_options[random_swim])
-    # if swim random int from swim array
-    
+    # random option
+    swim_result = print(swim_options[random_swim])
+    if swim_result == swim_options[0]:
+      print("Sorry buddy, you sunk like a stone.")
+      quit()
+    if swim_result == swim_options[1]:
+      print("Catching your breath on the shore you notice the sun will set soon.")
+      setting_sun = input("Would you like to continue walking or build shelter? Type walk/shelter: ").lower()
+      if setting_sun == "walk":
+        print("result from walking")
+    if swim_result == swim_options[2]:
+      answer = input("He reaches his hand down to you, you notice\
+        blood on his hand. Do you run or grab his hand or run? Type Grab/Run").lower()
+    if swim_result == swim_options[3]:
+      print("You wake up to the howling of wolves.")
+      quit()
+    if swim_result == swim_options[4]:
+      print("Your cramp gets serious once on shore.")
+      quit()
+    if swim_result == swim_options[5]:
+      print("Feeling confident and full of vigor\
+        you decide to sprint into the forest.")
+      quit()
+
+
   if answer == "walk":
     print()
 
 
 
+
+# user takes a right instead of a left
 elif answer == "right":
-  print()
+  answer = input("Right you are! While walking you enjoy a beautiful sunset, but you need to find shelter soon\
+    do you look for a town or build a shelter? Type town/shelter: ")
 
 else:
   print("Not a vaild option. You lose.")
